4b772df445f0a7b3150c5c4740643ec809cfd590
ahmedmohsen5 <a.khalefa555@gmail.com>
2026-09-13 14:54:58 +0300

    fix bootstrab

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -6,7 +6,7 @@ locals {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = local.state_bucket_name
-  force_destroy = false
+  force_destroy = true
 
   tags = {
     Name        = local.state_bucket_name
@@ -15,7 +15,34 @@ resource "aws_s3_bucket" "terraform_state" {
     managed_by  = "terraform"
   }
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+  
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
